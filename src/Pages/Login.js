662b9d6c7bc9606d3e3376ab86df89edefbdd7ac
Huy <kubin2712@gmail.com>
2021-01-07 18:47:26 +0700

    update

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -7,8 +7,8 @@ import CircularProgress from "@material-ui/core/CircularProgress";
 import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
-import { InputAdornment, Grid, Box } from "@material-ui/core";
-import { AccountCircle, LockRounded, PlayCircleFilledWhite } from "@material-ui/icons";
+import { InputAdornment, Grid, Box, Modal, Backdrop, Fade } from "@material-ui/core";
+import { AccountCircle, LockRounded, Email } from "@material-ui/icons";
 import truck from "../Image/truck.jpg"
 
 const useStyles = makeStyles((theme) => ({
@@ -37,7 +37,7 @@ const useStyles = makeStyles((theme) => ({
     paddingBottom: theme.spacing(5),
     paddingLeft: theme.spacing(10),
     paddingRight: theme.spacing(10),
-    paddingTop:'35vh',
+    paddingTop:'20vh',
   },
   form: {
     marginTop: theme.spacing(1),
@@ -48,9 +48,36 @@ const useStyles = makeStyles((theme) => ({
   button: {
       background: 'linear-gradient(45deg, rgba(255,228,132,1) 0%, rgba(255,202,24,1) 100%)',
   },
+  cancelbutton :  {
+                      background : 'linear-gradient(45deg, rgba(218, 223, 225, 1) 0%, rgba(218, 223, 225, 1) 100%)',
+                      position : 'inherit',
+                      width : '33%',
+                      marginTop : theme.spacing(12),
+                      left : theme.spacing(10),
+                  },
   textField: {
       //color: 'white',
-  }
+  },
+  registerbutton :  {
+                      background : 'linear-gradient(45deg, rgba(255,228,132,1) 0%, rgba(255,202,24,1) 100%)',
+                      position : 'inherit',
+                      width : '33%',
+                      marginTop : theme.spacing(12),
+                      right : theme.spacing(10),
+                    },
+  modal :   {
+              display : 'flex',
+              padding : theme.spacing(1),
+              alignItems : 'center',
+              justifyContent : 'center',
+            },
+  paper : {
+            backgroundColor: theme.palette.background.paper,
+            border: '2px solid #000',
+            boxShadow: theme.shadows[5],
+            padding: theme.spacing(2, 4, 3),
+            fontSize : '50px',
+          },
 }));
 
 function Login(props) {
@@ -59,15 +86,40 @@ function Login(props) {
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
-  const handleUsernameChange = (event) => {
-    //setIsTyping(true);
-    setUsername(event.target.value);
+  const [register,setRegister] = useState(false);
+  const rootRef = React.useRef(null);
+
+  const [reusername,setReusername] = useState("");
+  const [reemail,setReemail] = useState("");
+  const [repassword,setRepassword] = useState("");
+  const [reconfirmpassword,setReconfirmpassword] = useState("");
+
+  const [popup,setPopup] = useState(false);
+  const handleRegisterUsername = (event) =>{
+    setReusername(event.target.value);
+  };
+  const handleRegisterEmail = (event) =>{
+    setReemail(event.target.value);
+  };
+  const handleRegisterPassword = (event) =>{
+    setRepassword(event.target.value);
+  };
+  const handleRegisterConfirmpassword = (event) =>{
+    setReconfirmpassword(event.target.value);
   };
 
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+  function handleRegister(value)
+  {
+    setRegister(value);
+  }
   const handlePasswordChange = (event) => {
     //setIsTyping(true);
     setPassword(event.target.value);
   };
+
   // handle button click of login form
   const handleSubmit = () => {
     setError(null);
@@ -100,7 +152,42 @@ function Login(props) {
           setLoading(false);
         });
   };
+  const handleClosepopup = () =>{
+      setPopup(false);
+      setRegister(false);
+      setLoading(false);
+      props.history.push('/login');
+  }
+  const handleSubmitRegister = () => {
+    var data = {
+      reusername : reusername,
+      reemail : reemail,
+      repassword : repassword,
+      reconfirmpassword : reconfirmpassword,
 
+    };
+    setError(null);
+    setLoading(true);
+    axios .post("http://localhost:8080/users/register",data)
+          .then ((res) =>
+                        {
+                          if(res.data.status !== 0)
+                          {
+                            setLoading(false);
+                            setError(res.data.message);
+                          }
+                          else
+                          {
+                            setPopup(true);
+                          }
+                        }
+                )
+          .catch  ((error) => {
+                                setError("Something went wrond.");
+                                setLoading(false);
+                              }
+                  )
+  }
   return (
     <div className = {classes.body}>
     <Box
@@ -108,81 +195,226 @@ function Login(props) {
       boxShadow={4}
       borderColor = "white"
       className = {classes.box}
-    >
       
-      <Typography component="h1" variant="h4" p = {10}>
-        Sign in
-      </Typography>
-        <TextField
-          className = {classes.textField}
-          autoComplete='off'
-          variant="outlined"
-          margin="normal"
-          fullWidth
-          id="user"
-          label="Username"
-          name="user"
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <AccountCircle />
-              </InputAdornment>
-            ),
-          }}
-          onChange={handleUsernameChange}
-          autoFocus
-        />
+    >
+      { register !== true ? [
+        <Typography component="h1" variant="h4" p = {10}>
+          Sign in
+        </Typography>,
+          <TextField
+            className = {classes.textField}
+            autoComplete='off'
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            id="user"
+            label="Username"
+            name="user"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <AccountCircle />
+                </InputAdornment>
+              ),
+            }}
+            onChange={handleUsernameChange}
+            autoFocus
+          />,
 
-        <TextField
-          className = {classes.textField}
-          autoComplete='off'
-          variant="outlined"
-          margin="normal"
-          fullWidth
-          name="password"
-          label="Password"
-          type="password"
-          id="password"
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <LockRounded />
-              </InputAdornment>
+          <TextField
+            className = {classes.textField}
+            autoComplete='off'
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <LockRounded />
+                </InputAdornment>
+              ),
+            }}
+            onChange={handlePasswordChange}
+            autoComplete="current-password"
+            />,
+            loading === true ? (
+              <Button
+                disabled={true}
+                fullWidth
+                type="submit"
+                variant="contained"
+                color="primary"
+                name = 'Username'
+                lable = 'Username'
+                id = 'Username'
+              >
+                <CircularProgress />
+                Login
+              </Button>
+            ) : (
+              <Button
+                fullWidth
+                className = {classes.button}
+                type="submit"
+                variant="contained"
+                color="primary"
+                onClick={handleSubmit}
+              >
+                Login
+              </Button>
             ),
-          }}
-          onChange={handlePasswordChange}
-          autoComplete="current-password"
-        />
-        {loading === true ? (
-          <Button
-            disabled={true}
-            fullWidth
-            type="submit"
-            variant="contained"
-            color="primary"
-          >
-            <CircularProgress />
-            Login
-          </Button>
-        ) : (
-          <Button
-            fullWidth
-            className = {classes.button}
-            type="submit"
-            variant="contained"
-            color="primary"
-            onClick={handleSubmit}
-          >
-            Login
-          </Button>
-        )}
-    <Grid container justify="center">
-        <Grid item>
-          <Button> Forgot password?</Button>
-          <Button> Register</Button>
-        </Grid>
-      </Grid>
-      {error && <><small style={{ color: 'red' }}>{error}</small><br /></>}<br />
+            <Grid container justify="center">
+              <Grid item>
+                <Button> Forgot password?</Button>
+                <Button onClick = {() => handleRegister(true)}> Register</Button>
+              </Grid>
+            </Grid>,
+            error && <><small style={{ color: 'red' }}>{error}</small><br /></>,<br />,
+          ] :
+          [
+            <Typography component="h1" variant="h4" >
+              Register
+            </Typography>,
+            <TextField
+              autoComplete = 'off'
+              variant = 'outlined'
+              margin = 'normal'
+              fullWidth = {true}
+              id="reuser"
+              label="Username"
+              name="User"
+              InputProps= {{
+                startAdornment: (
+                                  <InputAdornment position="start">
+                                    <AccountCircle />
+                                  </InputAdornment>
+                                ),
+                          }}
+              onChange={handleRegisterUsername}
+              autoFocus
+            />,
+            <TextField
+              autoComplete = 'off'
+              variant = 'outlined'
+              fullWidth = {true}
+              id="reemail"
+              margin = 'normal'
+              label="Email"
+              name="Email"
+              InputProps= {{
+                startAdornment: (
+                                  <InputAdornment position="start">
+                                    <Email />
+                                  </InputAdornment>
+                                ),
+                          }}
+              onChange={handleRegisterEmail}
+              autoFocus
+            />,
+            <TextField
+              autoComplete = 'off'
+              variant = 'outlined'
+              fullWidth = {true}
+              margin = 'normal'
+              id="repassword"
+              label="Password"
+              name="Password"
+              type="password"
+              InputProps= {{
+                startAdornment: (
+                                  <InputAdornment position="start">
+                                    <LockRounded />
+                                  </InputAdornment>
+                                ),
+                          }}
+              onChange={handleRegisterPassword}
+              autoFocus
+            />,
+            <TextField
+              autoComplete = 'off'
+              variant = 'outlined'
+              fullWidth = {true}
+              margin = 'normal'
+              id="reconfirmpassword"
+              label="Confirm Password"
+              name="ConfirmPassword"
+              type="password"
+              InputProps= {{
+                startAdornment: (
+                                  <InputAdornment position="start">
+                                    <LockRounded />
+                                  </InputAdornment>
+                                ),
+                          }}
+              onChange={handleRegisterConfirmpassword}
+              autoFocus
+            />,
+            loading !== true ?[
+              <Button
+                className = {classes.cancelbutton}
+                type="Cancle"
+                variant="contained"
+                color="primary"
+                onClick={() => handleRegister(false)}
+              >
+                Cancel
+              </Button>,
+              <Button
+                className = {classes.registerbutton}
+                type="Register"
+                variant="contained"
+                color="primary"
+                onClick={handleSubmitRegister}
+              >
+                Register
+              </Button>,
+            ]:[
+                <Button
+                  disabled
+                  className = {classes.cancelbutton}
+                  type="Cancle"
+                  variant="contained"
+                  color="primary"
+                >
+                  Cancel
+                </Button>,
+                <Button
+                  disabled
+                  className = {classes.registerbutton}
+                  type="Register"
+                  variant="contained"
+                  color="primary"
+                >
+                  <CircularProgress />
+                  Register
+                </Button>,
+            ],
+            <Modal
+              aria-labelledby="transition-modal-title"
+              aria-describedby="transition-modal-description"
+              className={classes.modal}
+              open={popup}
+              onClose={handleClosepopup}
+              closeAfterTransition
+              BackdropComponent={Backdrop}
+              BackdropProps={ {
+                                timeout: 500,
+                              }
+                            }
+            >
+              <Fade in={popup}>
+                <div className={classes.paper}>
+                  <p id="transition-modal-description">Successful</p>
+                </div>
+              </Fade>
+            </Modal>,
+            error && <><small style={{ color: 'red' }}>{error}</small><br /></>,<br />,
+          ]
+        }
     </Box>
     </div>
   );
